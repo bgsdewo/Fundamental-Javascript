aa42a72664fb42d97ee87590d1f5eb4988faed3f
unknown <bgsdewoo20@gmail.com>
2024-04-22 22:31:59 +0700

    Belajar mengakses objec didalam array

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-let score;
-score = 10;
-console.log(score)
+console.log('CARA MENGAKSES OBJEC DIDALAM ARRAY')
+
+
+const shopingCart = [
+    {
+        product: 'vue.js',
+        price: 1.22,
+        quantity: 33,
+    },
+    {
+        product: 'REACT.js',
+        price: 2.32,
+        quantity: 12,
+    },
+]
+
+console.log(shopingCart[0].quantity)
