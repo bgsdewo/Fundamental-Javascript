2a59d587521e9489e86eb0383d4c633dd814adb9
unknown <bgsdewoo20@gmail.com>
2024-04-23 08:03:34 +0700

    belajar perulangan for

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-console.log('CARA MENGAKSES OBJEC DIDALAM ARRAY')
+// console.log('contoh penggunaan dari perulangan for')
+
+// for (let i = 20;i>= 0;i--) {
+//     console.log('Ini contoh pengurangan')
+// }
+
+// for (let i = 0; i<= 20; i++) {
+//     console.log('ini contoh penambahan')
+// }
 
 
-const shopingCart = [
-    {
-        product: 'vue.js',
-        price: 1.22,
-        quantity: 33,
-    },
-    {
-        product: 'REACT.js',
-        price: 2.32,
-        quantity: 12,
-    },
+// //setiap melakukan perulangan dengan array,selalu awali nilai dengan 0 dan batasnya sampai panjangnya suatu data array yg akan digunakan
+// const animals = ['Serigala','Macan','Singa','Burung']
+
+// for (let i= 0; i<animals.length;i++) {
+//     console.log(i+1,animals[i])
+// }
+
+// //contoh penggunaan dari nested loop 
+// let soal = ['A','B','C','D','E']
+// for (let i = 1; i <10; i++) {
+//     console.log(`${i} ini adalah soal nomor ${i}`)
+//     for(let j = 0; j<soal.length; j++) {
+//         console.log(`  ${soal[j]}. Pilihan jawaban`)
+//     }
+// }
+
+//CARA MEMANFAATKAN ARRAY DALAM ARRAY DENGAN NESTED LOOP
+
+const studentRow = [
+    ['Syifa','Mikael','Naruto'],
+    ['Milo','Mikel','Ruto'],
+    ['Yifa','Mika','Nato']
 ]
 
-console.log(shopingCart[0].quantity)
+for(let i  = 0;i < studentRow.length;i++){
+    const row = studentRow[i];
+    console.log(`Seat row ${studentRow}`)
+    for(let j = 0; j<row.length;j++){
+        console.log(` ${row[j]}`)
+    }
+}
